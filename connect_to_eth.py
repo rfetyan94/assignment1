c3af97d89de1ba64311cece34e7cac274604b45f
rfetyan94 <rfetyan@seas.upenn.edu>
2025-06-30 09:28:11 -0600

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -25,7 +25,7 @@ def connect_with_middleware(contract_json):
 		abi = d['abi']
 
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
-	url = "https://data-seed-prebsc-1-s1.binance.org:8545/"
+	url = "https://eth-mainnet.g.alchemy.com/v2/LgjiTyR8VGX0vSKwgZkDdXlBchzkM8Zj/getNFTs/?owner=vitalik.eth"
 	w3 = Web3(HTTPProvider(url))
 	assert w3.is_connected(), f"Failed to connect to provider at {url}"
 
