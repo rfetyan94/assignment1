3c3c68f32c153ac3db9f6ca7d4ad15934033039e
rfetyan94 <rfetyan@seas.upenn.edu>
2025-06-30 09:31:27 -0600

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -11,7 +11,7 @@ infura_url  = f"https://mainnet.infura.io/v3/{infura_token}"
 '''
 
 def connect_to_eth():
-	url = "https://eth-mainnet.g.alchemy.com/v2/LgjiTyR8VGX0vSKwgZkDdXlBchzkM8Zj"
+  url = "https://eth-mainnet.g.alchemy.com/v2/LgjiTyR8VGX0vSKwgZkDdXlBchzkM8Zj"
   w3 = Web3(HTTPProvider(url))
 	assert w3.is_connected(), f"Failed to connect to provider at {url}"
 	return w3
