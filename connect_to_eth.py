f55809dcd8245cd26fd9ae8079b5deb61d786b61
rfetyan94 <rfetyan@seas.upenn.edu>
2025-06-29 18:35:15 -0600

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -11,7 +11,7 @@ infura_url  = f"https://mainnet.infura.io/v3/{infura_token}"
 '''
 
 def connect_to_eth():
-	url = "https://bsc-dataseed.binance.org/"  # FILL THIS IN (for example, Infura or Alchemy Ethereum Mainnet)
+	url = "https://data-seed-prebsc-1-s1.binance.org:8545/"  # FILL THIS IN (for example, Infura or Alchemy Ethereum Mainnet)
 	w3 = Web3(HTTPProvider(url))
 	assert w3.is_connected(), f"Failed to connect to provider at {url}"
 	return w3
