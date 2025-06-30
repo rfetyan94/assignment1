bfcb28dc0a9acce83bb0237b910af4a5dd353a0b
rfetyan94 <rfetyan@seas.upenn.edu>
2025-06-30 09:34:13 -0600

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -11,32 +11,32 @@ infura_url  = f"https://mainnet.infura.io/v3/{infura_token}"
 '''
 
 def connect_to_eth():
-  url = "https://eth-mainnet.g.alchemy.com/v2/LgjiTyR8VGX0vSKwgZkDdXlBchzkM8Zj"
-  w3 = Web3(HTTPProvider(url))
-	assert w3.is_connected(), f"Failed to connect to provider at {url}"
-	return w3
+    url = "https://eth-mainnet.g.alchemy.com/v2/LgjiTyR8VGX0vSKwgZkDdXlBchzkM8Zj"
+    w3 = Web3(HTTPProvider(url))
+    assert w3.is_connected(), f"Failed to connect to provider at {url}"
+    return w3
 
 
 def connect_with_middleware(contract_json):
-	with open(contract_json, "r") as f:
-		d = json.load(f)
-		d = d['bsc']
-		address = d['address']
-		abi = d['abi']
+    with open(contract_json, "r") as f:
+        d = json.load(f)
+        d = d['bsc']
+        address = d['address']
+        abi = d['abi']
 
-	# The first section will be the same as "connect_to_eth()" but with a BNB url
-	url = "https://eth-mainnet.g.alchemy.com/v2/LgjiTyR8VGX0vSKwgZkDdXlBchzkM8Zj"
-	w3 = Web3(HTTPProvider(url))
-	assert w3.is_connected(), f"Failed to connect to provider at {url}"
+    # The first section will be the same as "connect_to_eth()" but with a BNB url
+    url = "https://eth-mainnet.g.alchemy.com/v2/LgjiTyR8VGX0vSKwgZkDdXlBchzkM8Zj"
+    w3 = Web3(HTTPProvider(url))
+    assert w3.is_connected(), f"Failed to connect to provider at {url}"
 
-	# The second section requires you to inject middleware into your w3 object and
-	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
-	# and https://web3py.readthedocs.io/en/stable/web3.contract.html
-	w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
-	contract = w3.eth.contract(address=Web3.to_checksum_address(address), abi=abi)
+    # The second section requires you to inject middleware into your w3 object and
+    # create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
+    # and https://web3py.readthedocs.io/en/stable/web3.contract.html
+    w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
+    contract = w3.eth.contract(address=Web3.to_checksum_address(address), abi=abi)
 
-	return w3, contract
+    return w3, contract
 
 
 if __name__ == "__main__":
-  connect_to_eth()
+    connect_to_eth()
